e2713af9a0c04ccd64d330435b9a9cc6344b1ebb
yousufatef <joeatef769@gmail.com>
2025-04-26 20:17:41 +0300

    comment seeding function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,22 @@
-import { PrismaClient } from '@prisma/client'
-import { faker } from '@faker-js/faker';
+// import { PrismaClient } from '@prisma/client'
+// import { faker } from '@faker-js/faker';
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-async function main() {
-    await prisma.todo.createMany({
-        data: Array.from({ length: 20 }, () => ({
-            title: faker.lorem.text(),
-            body: faker.lorem.paragraph(),
-        })),
-    });
-}
+// async function main() {
+//     await prisma.todo.createMany({
+//         data: Array.from({ length: 20 }, () => ({
+//             title: faker.lorem.text(),
+//             body: faker.lorem.paragraph(),
+//         })),
+//     });
+// }
 
-main()
-    .catch(async (e) => {
-        console.error(e)
-        process.exit(1)
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+// main()
+//     .catch(async (e) => {
+//         console.error(e)
+//         process.exit(1)
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect()
+//     })
